ce04f1648ab365bb1bb892dffec753e526ec6a9d
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-10-06 13:15:10 -0700

    a lot

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+  {
+    routeName: 'chaz',
+    name: 'Chaz McChazington',
+    choice: ['5', '3', '1', '5', '3', '2', '5', '1', '4', '3']
+  },
+  {
+    routeName: 'lee',
+    name: 'lee teelee',
+    choice: ['1', '2', '4', '3', '4', '3', '4', '1', '1', '3']
+  },
+  {
+    routeName: 'dog',
+    name: 'Max',
+    choice: ['3', '4', '5', '2', '1', '5', '5', '5', '5', '5']
+  }
+
+];
+
+module.exports = friends;
