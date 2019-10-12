de87bd7a4585bcf78b97d79e697acfff4c1831a8
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-10-12 13:23:25 -0700

    turn in

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,6 +3,8 @@ var friends= require('../data/friends.js');
 module.exports =  function (app) {
     app.get('/api/friends', function(req, res) {
         res.json(friends);
+        var alldata = res.json(friends);
+        console.log(alldata)
     });
 
     app.post('/api/friends', function(req,res) {
@@ -12,19 +14,20 @@ module.exports =  function (app) {
             photo: '',
             choiceDifference: 0
         };
-
         var userData = req.body;
         var userScores = userData.choice;
+        console.log('userScore = ' + userScores)
+        var choiceDifference = 0;
        
-        var choiceDifference;
         
         for (var i = 0; i < friends.length; i++) {
-            choiceDifference = 0;
+           
             for (var j = 0; j < friends[i].choice.length; j++) {
                 
-                choiceDifference = Math.abs(parseInt(userScores[j]) - parseInt(friends[i].choice[j]));
+                choiceDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].choice[j]));
                 
-                if (choiceDifference === yourFriend.choiceDifference) {
+                
+                if (choiceDifference >= yourFriend.choiceDifference) {
                     yourFriend.name = friends[i].name;
                     yourFriend.photo = friends[i].photo;
                     yourFriend.friendDifference = choiceDifference;
