c9c63cdbb5e7dae9efd0f420b724d4967372709f
Darioush Mansourzadeh <darioush2011@gmail.com>
2020-03-17 21:40:08 -0700

    connecting style

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const path = require('path')
+// Tells node that we are creating an "express" server
+var app = express();
+
+// // Sets an initial port. We"ll use this later in our listener
+// var PORT = process.env.PORT || 8080;
+
+// // express.json and express.urlEncoded make it easy for our server to interpret data sent to it.
+// // The code below is pretty standard.
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
 var bodyParser = require('body-parser')
-
-
-const app = express();
 
 
 const PORT = 3000;
@@ -19,6 +27,9 @@ app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
  
 // parse an HTML body into a string
 app.use(bodyParser.text({ type: 'text/html' }))
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
